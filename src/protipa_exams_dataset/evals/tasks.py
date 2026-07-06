328b9e667d418e1197ac36095f7cdfa67e1ae9bd
PK9811-hub <p.kyriazi@athenarc.gr>
2026-07-06 17:17:50 +0300

    Updated logs

--- src/protipa_exams_dataset/evals/tasks.py
+++ src/protipa_exams_dataset/evals/tasks.py
@@ -45,6 +45,15 @@ def generic_evaluation(
     except Exception as e:
         print(f"Warning: Could not load few-shot split '{fewshot_split}'. Proceeding with 0 shots.")
         dev_records = []
+        
+    # 1. Φορτώνουμε το dev split ΜΟΝΟ αν ζητηθούν few-shot παραδείγματα
+    #dev_records = []
+    #if num_fewshot > 0:
+        #try:
+            #dev_data = load_dataset(dataset_path, dataset_name, split=fewshot_split)
+            #dev_records = list(dev_data)
+        #except Exception as e:
+            #print(f"Warning: Could not load few-shot split '{fewshot_split}'. Proceeding with 0 shots.")
 
     def record_to_sample(x):
         subject = x.get("subject")
